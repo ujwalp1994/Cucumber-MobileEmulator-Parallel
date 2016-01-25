b71bf13eba4510293c5e800ecf8d53380afaa8aa
Ashwin Gonsalves <ashwin@Ashwins-MacBook-Pro.local>
2016-01-25 23:03:55 +0530

    First commit

---
+++ cucumber/parallel/Rakefile
@@ -0,0 +1,28 @@
+require 'rubygems'
+require 'cucumber'
+require 'cucumber/rake/task'
+require 'parallel'
+require 'json'
+
+@browsers = JSON.load(open('browsers.json'))
+@parallel_limit = ENV["nodes"] || 1
+@parallel_limit = @parallel_limit.to_i
+
+task :cucumber do
+  Parallel.each(@browsers, :in_processes => @parallel_limit) do |browser|
+    begin
+      puts "Running with: #{browser.inspect}"
+      ENV['SELENIUM_BROWSER'] = browser['browser']
+      ENV['SELENIUM_VERSION'] = browser['browser_version']
+      ENV['BS_AUTOMATE_OS'] = browser['os']
+      ENV['BS_AUTOMATE_OS_VERSION'] = browser['os_version']
+
+      Rake::Task[:run_features].execute()
+    rescue Exception => e
+      puts "Error while running task"
+    end
+  end
+end
+
+Cucumber::Rake::Task.new(:run_features)
+task :default => [:cucumber]
---
+++ cucumber/parallel/features/support/env.rb
@@ -0,0 +1,24 @@
+require 'selenium/webdriver'
+
+url = "http://#{ENV['BS_USERNAME']}:#{ENV['BS_AUTHKEY']}@hub.browserstack.com/wd/hub"
+
+capabilities = Selenium::WebDriver::Remote::Capabilities.new
+capabilities['os'] = ENV['BS_AUTOMATE_OS']
+capabilities['os_version'] = ENV['BS_AUTOMATE_OS_VERSION']
+capabilities['browser'] = ENV['SELENIUM_BROWSER']
+capabilities['browser_version'] = ENV['SELENIUM_VERSION']
+capabilities['browserstack.debug'] = "true"
+
+capabilities['project'] = ENV['BS_AUTOMATE_PROJECT'] if ENV['BS_AUTOMATE_PROJECT']
+capabilities['build'] = ENV['BS_AUTOMATE_BUILD'] if ENV['BS_AUTOMATE_BUILD']
+
+browser = Selenium::WebDriver.for(:remote, :url => url,
+                                  :desired_capabilities => capabilities)
+
+Before do |scenario|
+  @browser = browser
+end
+
+at_exit do
+  browser.quit
+end
